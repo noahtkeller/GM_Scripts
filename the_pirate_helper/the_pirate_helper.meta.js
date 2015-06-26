8a82982907d00d35e9fde2f6fbf0e8ed08f27241
Noah Keller <noahtkeller@gmail.com>
2015-06-26 17:30:02 -0400

    New version

---
+++ the_pirate_helper/the_pirate_helper.meta.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name           The Pirate Helper
+// @description    Enhances your pirating experience!
+// @author         Noah Keller http://github.com/noahtkeller
+// @version        6
+// @encoding       utf-8
+// @namespace      http://github.com/noahtkeller
+// @updateURL      https://raw.github.com/noahtkeller/GM_Scripts/the_pirate_helper.meta.js
+// @downloadURL    https://raw.github.com/noahtkeller/GM_Scripts/the_pirate_helper.user.js
+// @date           2013-12-26
+// @source         http://userscripts.org/scripts/show/56244
+// @identifier     http://userscripts.org/scripts/source/56244.user.js
+// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require        https://raw.github.com/noahtkeller/GM_Scripts/master/userscripts.lib.js
+// @include        http*://thepiratebay.*
+// @grant          GM_xmlhttpRequest
+// @grant          GM_setValue
+// @grant          GM_getValue
+// @grant          GM_info
+// @grant          GM_registerMenuCommand
+// @run-at document-end
+// @noframes
+// ==/UserScript==
